f11ee2c9c3cd26b23e70d66dfb9afdcc4ccec472
Simon Marenkov <simon.marenkov@gmail.com>
2025-02-16 00:13:07 +0200

    Fix kill zone

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -2,7 +2,8 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 
-func _on_body_entered(body: CharacterBody2D) -> void:
+func _on_body_entered(body) -> void:
+	print("Someone reach bottom kill zone" + str(body))
 	timer.start()
 
 
